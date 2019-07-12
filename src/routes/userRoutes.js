1f4e1be8659c609d8bed58cc1df91999f7b92397
stenioaguiar <40396667+stenioaguiar@users.noreply.github.com>
2019-07-12 16:53:19 -0300

    add get user data //accounts{accounts}

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -37,12 +37,27 @@ class UserRoutes extends BaseRoute {
                         message: 'Password incorrect!'
                     })
                 }         
+                const userToken = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = (d + Math.random() * 16) % 16 | 0;
+                    d = Math.floor(d / 16);
+                    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+                });
+
+                const d = new Date().getTime();
+                if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+                    d += performance.now(); //use high-precision timer if available
+                }
+                const token = ( 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    const r = (d + Math.random() * 16) % 16 | 0;
+                    d = Math.floor(d / 16);
+                    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+                }))
 
                 const userData = {
                     account: account[0].account,
                     name: account[0].name,
                     balance: account[0].balance,
-                    token: 'e0ce3be0-b650-4b4c-ab23-70abd8757058'
+                    userToken
                 }
                 return userData;
             }
@@ -111,6 +126,43 @@ class UserRoutes extends BaseRoute {
             }
         }
     }
+    userdata() {
+        return {
+            path: '/accounts/{account}',
+            method: 'GET',
+            config: {
+                validate: {
+                    failAction: (request, h, err) => {
+                        throw err;
+                      },
+                    params: {
+                        account: Joi.string().required()
+                    }
+                },
+            },            
+            handler: async (request, headers) => {
+                
+                const item = await this.UserDb.read({account: request.params.account},1)
+                console.log('item',item)
+
+                if(item.length == 0){
+                    return {
+                        response: 'false',
+                        message: 'Usuário não encontrado'
+                    }
+                }
+                const itemData = {
+                    name: item[0].name,
+                    account: item[0].account
+                }
+                return {
+                    response: 'true',
+                    message: itemData
+                }
+            }
+        }
+    }
+
     update() {
         return {
             path: '/user/{account}',
